9f44fd547c1667f3978fab8c2e938b9b751d11f3
Will Abson <will.abson@gmail.com>
2018-11-23 19:17:20 +0000

    Add dj-static for serving static files

--- dwgraph/wsgi.py
+++ dwgraph/wsgi.py
@@ -29,7 +29,8 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "dwgraph.settings"
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
